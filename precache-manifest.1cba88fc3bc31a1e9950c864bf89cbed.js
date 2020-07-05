29e325a5ddb0ee591eb0508fc8fa3350f642128b
samitpal2000 <sammit.dp.pal2000@gmail.com>
2020-07-05 21:38:27 +0530

    Updates

---
+++ precache-manifest.1cba88fc3bc31a1e9950c864bf89cbed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695edc9ce36dfec39d9bd3a2e4ab6b94",
+    "url": "/imagedetection/index.html"
+  },
+  {
+    "revision": "6fcddc6e81452172f3f0",
+    "url": "/imagedetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1a5aab18d36f8ad1b4f4",
+    "url": "/imagedetection/static/css/main.73d79230.chunk.css"
+  },
+  {
+    "revision": "6fcddc6e81452172f3f0",
+    "url": "/imagedetection/static/js/2.8f58e6b8.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/imagedetection/static/js/2.8f58e6b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5aab18d36f8ad1b4f4",
+    "url": "/imagedetection/static/js/main.83f49ce9.chunk.js"
+  },
+  {
+    "revision": "7b7f31a0b7a6623e007b",
+    "url": "/imagedetection/static/js/runtime-main.3c4f05a1.js"
+  }
+]);
